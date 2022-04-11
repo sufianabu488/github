4171a172898c760aca670f17b1be011c265c9fdb
sufianabu488 <sufianabu488@gmail.com>
2022-04-10 20:21:16 -0400

    first commut

---
+++ Github_Hw1.java
@@ -0,0 +1,21 @@
+package github;
+
+public class Github_Hw1 {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=200;
+		char h='k';
+		double d=2000.20;
+		boolean f=false;
+		String name="abu";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(h);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(name);
+
+	}
+
+}
